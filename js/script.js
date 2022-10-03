1c2944763a0e88313952416149edb63ba92f0898
coko <89577404+cokocoio@users.noreply.github.com>
2022-10-03 23:33:56 +0900

    0.2beta

--- js/script.js
+++ js/script.js
@@ -12,9 +12,95 @@ function setIntRandom(min, max,amount) {
     console.log(randnum);
     return randnum;
 }
+/**
+ * 
+ * @param {number} min 最小値
+ * @param {number} max 最大値
+ * @returns {boolean}
+ */
+function isMinMax(min, max) {   
+    if(min > max) {
+        console.log("min > max"); //invalid
+        return false;
+    }else{
+        console.log("min <= max"); //correct
+        return true;
+    }
+}
+/**
+ * @param {number} x
+ */
+function isNumber(x){
+    if(x != NaN) {
+        return true;
+    }else{
+        return false;
+    }
+}
+/**
+ * 
+ * @returns {{min: number, max: number, amount: number}} 
+ * min 最小値 max 最大値 amount 個数
+ */
+function getFormValue() {
+    let min = Number(document.getElementById("min").value);
+    let max = Number(document.getElementById("max").value);
+    let amount = Number(document.getElementById("amount").value);
+    return [min,max,amount];
+}
+/**
+ * ボタンクリック時の処理
+ */
+function buttonClick() {
+    let min = getFormValue()[0];
+    let max = getFormValue()[1];
+    let amount = getFormValue()[2];
+    let randnum = setIntRandom(min, max, amount);
+    for(let i = 0; i < amount; i++) {
+        document.getElementById("export-box").append(randnum[i] + " ");
+    }
+}
+/**
+ * 
+ * @param {string} errorMessage 
+ * @param {string} messagePlace 
+ */
+function showErrorMessage(errorMessage, messagePlace){
+    document.getElementById(messagePlace).innerHTML = errorMessage;
+}
+/**
+ * 
+ * @param {string} messagePlace 
+ */
+function clearErrorMessage(messagePlace){
+    document.getElementById(messagePlace).innerHTML = "";
+}
+/**
+ * formの最小値と最大値のチェック
+ */
+function formMinMaxCheck() {
+    let min = getFormValue()[0];
+    let max = getFormValue()[1];
+    //error pattern 1 "min max check"
+    if(isMinMax(min, max)) { //min <= max
+        document.getElementById("min").classList.remove("error");
+        document.getElementById("max").classList.remove("error");
+        console.log("min <= max");
+        clearErrorMessage("error-message-1");
+        document.getElementById("rand-button").disabled = false;
+    }else{ //min > max
+        document.getElementById("min").classList.add("error");
+        document.getElementById("max").classList.add("error");
+        console.log("min > max");
+        showErrorMessage("最小値は最大値以下にしてください。", "error-message-1");
+        document.getElementById("rand-button").disabled = true;
+    }
+    
+}
+
 window.onload = function() {
     /* set version */
-    let version = "0.1β";
+    let version = "0.2β";
     document.title = "乱数メーカー " + version;
     document.getElementById("version").innerHTML = version;
     /* set copyright year */
@@ -29,14 +115,16 @@ window.onload = function() {
     }
     document.getElementById("copyright-year").innerHTML = YEAR_SPAN;
     /*button event*/
-    document.getElementById("rand-button").addEventListener("click", function() {
-        let min = Number(document.getElementById("min").value);
-        let max = Number(document.getElementById("max").value);
-        let amount = Number(document.getElementById("amount").value);
-        let randnum = setIntRandom(min, max, amount);
-        for(let i = 0; i < amount; i++) {
-            document.getElementById("export-box").append(randnum[i] + " ");
-        }
-                //document.getElementById("export-box").innerHTML = randnum;
+    document.getElementById("rand-button").addEventListener("click", buttonClick);
+    /*form check*/
+    document.getElementById("min").addEventListener("input", function() {
+        formMinMaxCheck();
+    });
+    document.getElementById("max").addEventListener("input", function() {
+        formMinMaxCheck();
+    });
+    /*test code*/
+    document.getElementById("test").addEventListener("click", function() {
+        //document.getElementById("min").classList.add("error");
     });
 }
