3ff03a84c3ed954258757240fa2733742f017c7f
coko <89577404+cokocoio@users.noreply.github.com>
2022-10-08 23:11:12 +0900

    0.3beta

--- js/script.js
+++ js/script.js
@@ -39,6 +39,18 @@ function isNumber(x){
 }
 /**
  * 
+ * @param {string} str 
+ * @returns boolean
+ */
+ function isEmpty(str) {
+    if(str == "") {
+        return true;
+    }else{
+        return false;
+    }
+}
+/**
+ * 
  * @returns {{min: number, max: number, amount: number}} 
  * min 最小値 max 最大値 amount 個数
  */
@@ -56,23 +68,27 @@ function buttonClick() {
     let max = getFormValue()[1];
     let amount = getFormValue()[2];
     let randnum = setIntRandom(min, max, amount);
+    let isCheck = document.getElementById("chk-clear").checked;
+    if(isCheck) {
+        document.getElementById("export-box").innerHTML = "";
+    }    
     for(let i = 0; i < amount; i++) {
         document.getElementById("export-box").append(randnum[i] + " ");
     }
 }
 /**
  * 
- * @param {string} errorMessage 
+ * @param {string} message
  * @param {string} messagePlace 
  */
-function showErrorMessage(errorMessage, messagePlace){
-    document.getElementById(messagePlace).innerHTML = errorMessage;
+function showMessage(message, messagePlace){
+    document.getElementById(messagePlace).innerHTML = message;
 }
 /**
  * 
  * @param {string} messagePlace 
  */
-function clearErrorMessage(messagePlace){
+function clearMessage(messagePlace){
     document.getElementById(messagePlace).innerHTML = "";
 }
 /**
@@ -86,21 +102,39 @@ function formMinMaxCheck() {
         document.getElementById("min").classList.remove("error");
         document.getElementById("max").classList.remove("error");
         console.log("min <= max");
-        clearErrorMessage("error-message-1");
-        document.getElementById("rand-button").disabled = false;
+        clearMessage("error-message-1");
+        return true; //OK
+        //document.getElementById("rand-button").disabled = false;
     }else{ //min > max
         document.getElementById("min").classList.add("error");
         document.getElementById("max").classList.add("error");
-        console.log("min > max");
-        showErrorMessage("最小値は最大値以下にしてください。", "error-message-1");
-        document.getElementById("rand-button").disabled = true;
+        console.log("min > max")
+        showMessage("最小値は最大値以下にしてください。", "error-message-1");
+        return false; //NG
     }
     
 }
-
+/**
+ * CHK is empty
+ * true: some empty
+ * false: no one empty
+ * @returns {boolean}
+ */
+function checkEmpty() {
+    let min = getFormValue()[0];
+    let max = getFormValue()[1];
+    let amount = getFormValue()[2];
+    if(isEmpty(min) || isEmpty(max) || isEmpty(amount)) {
+        //some input is empty
+        return true; //NG
+    }else{
+        //all input is not empty
+        return false; //OK
+    }
+}
 window.onload = function() {
     /* set version */
-    let version = "0.2β";
+    let version = "0.3β";
     document.title = "乱数メーカー " + version;
     document.getElementById("version").innerHTML = version;
     /* set copyright year */
@@ -118,10 +152,25 @@ window.onload = function() {
     document.getElementById("rand-button").addEventListener("click", buttonClick);
     /*form check*/
     document.getElementById("min").addEventListener("input", function() {
-        formMinMaxCheck();
+        if(formMinMaxCheck() && !checkEmpty()) {
+            document.getElementById("rand-button").disabled = false;
+        }else{
+            document.getElementById("rand-button").disabled = true;
+        }
     });
     document.getElementById("max").addEventListener("input", function() {
-        formMinMaxCheck();
+        if(formMinMaxCheck() && !checkEmpty()) {
+            document.getElementById("rand-button").disabled = false;
+        }else{
+            document.getElementById("rand-button").disabled = true;
+        }
+    });
+    document.getElementById("amount").addEventListener("input", function() {
+        if(formMinMaxCheck() && !checkEmpty()) {
+            document.getElementById("rand-button").disabled = false;
+        }else{
+            document.getElementById("rand-button").disabled = true;
+        }
     });
     /*test code*/
     document.getElementById("test").addEventListener("click", function() {
