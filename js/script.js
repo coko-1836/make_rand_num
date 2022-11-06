e956bfbc47381016e741cea01213d1843893b3e0
coko <89577404+cokocoio@users.noreply.github.com>
2022-11-06 23:36:01 +0900

    0.4-beta

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*汎用*/
 /**
  * 
  * @param {number} min 最小値
@@ -9,7 +10,7 @@ function setIntRandom(min, max,amount) {
     for(let i = 0; i < amount; i++) {
         randnum[i] = Math.floor(Math.random() * (max - min + 1)) + min;
     }
-    console.log(randnum);
+    //console.log(randnum);
     return randnum;
 }
 /**
@@ -20,10 +21,10 @@ function setIntRandom(min, max,amount) {
  */
 function isMinMax(min, max) {   
     if(min > max) {
-        console.log("min > max"); //invalid
+        //console.log("min > max"); //invalid
         return false;
     }else{
-        console.log("min <= max"); //correct
+        //console.log("min <= max"); //correct
         return true;
     }
 }
@@ -61,6 +62,53 @@ function getFormValue() {
     return [min,max,amount];
 }
 /**
+ * set cookie
+ * @param {number} min 最小値
+ * @param {number} max 最大値
+ * @param {number} amount 個数
+ */
+function setCookie(min,max,amount) {
+    document.cookie = "min=" + min +";sameSite=strict";
+    document.cookie = "max=" + max +";sameSite=strict";
+    document.cookie = "amount=" + amount +";sameSite=strict";
+    console.log(document.cookie);
+}
+/**
+ * 
+ * @returns {number} min max amount
+ */
+function getCookie(){
+    let cookie = document.cookie.split(";");
+    let min;
+    let max;
+    let amount;
+    cookie.forEach(e => {
+        if(e.trim().startsWith("min=")) {
+            min = e.trim().split("=")[1];
+        }
+        if(e.trim().startsWith("max=")) {
+            max = e.trim().split("=")[1];
+        }
+        if(e.trim().startsWith("amount=")) {
+            amount = e.trim().split("=")[1];
+        }
+    });
+    // (typeof min == "undefined") ? min = "" : min = min;
+    // (typeof max == "undefined") ? max = "" : max = max;
+    // (typeof amount == "undefined") ? amount = "" : amount = amount;
+    console.log(min,max,amount);
+    return [min,max,amount];
+}
+/**
+ * delete cookie
+ */
+function deleteCookie() {
+    document.cookie = "min=;max-age=0;";
+    document.cookie = "max=;max-age=0;";
+    document.cookie = "amount=;max-age=0;";
+}
+/*特有*/
+/**
  * ボタンクリック時の処理
  */
 function buttonClick() {
@@ -69,12 +117,18 @@ function buttonClick() {
     let amount = getFormValue()[2];
     let randnum = setIntRandom(min, max, amount);
     let isCheck = document.getElementById("chk-clear").checked;
+    let isDelCookieCheck = document.getElementById("chk-del-cookie").checked;
     if(isCheck) {
         document.getElementById("export-box").innerHTML = "";
-    }    
+    }
     for(let i = 0; i < amount; i++) {
         document.getElementById("export-box").append(randnum[i] + " ");
     }
+    setCookie(min,max,amount);
+    if(isDelCookieCheck) {
+        deleteCookie();
+    }
+    
 }
 /**
  * 
@@ -101,14 +155,14 @@ function formMinMaxCheck() {
     if(isMinMax(min, max)) { //min <= max
         document.getElementById("min").classList.remove("error");
         document.getElementById("max").classList.remove("error");
-        console.log("min <= max");
+        //console.log("min <= max");
         clearMessage("error-message-1");
         return true; //OK
         //document.getElementById("rand-button").disabled = false;
     }else{ //min > max
         document.getElementById("min").classList.add("error");
         document.getElementById("max").classList.add("error");
-        console.log("min > max")
+        //console.log("min > max")
         showMessage("最小値は最大値以下にしてください。", "error-message-1");
         return false; //NG
     }
@@ -132,9 +186,24 @@ function checkEmpty() {
         return false; //OK
     }
 }
+function setValueFromCookie(){
+    let min = getCookie()[0];
+    let max = getCookie()[1];
+    let amount = getCookie()[2];
+    console.log(min,max,amount);
+    if(min != ""){
+        document.getElementById("min").value = min;
+    }
+    if(max != ""){
+        document.getElementById("max").value = max;
+    }
+    if(amount != ""){
+        document.getElementById("amount").value = amount;
+    }
+}
 window.onload = function() {
     /* set version */
-    let version = "0.3β";
+    let version = "0.4β";
     document.title = "乱数メーカー " + version;
     document.getElementById("version").innerHTML = version;
     /* set copyright year */
@@ -142,12 +211,15 @@ window.onload = function() {
     let YEAR_SPAN = null;
     let date = new Date();
     let nowYear = date.getFullYear();
-    if(nowYear != START_YEAR) {
-        YEAR_SPAN = START_YEAR + " - " + nowYear;
-    } else {
-        YEAR_SPAN = START_YEAR;
-    }
+    (nowYear == START_YEAR) ? YEAR_SPAN = START_YEAR : YEAR_SPAN = START_YEAR + " - " + nowYear;
     document.getElementById("copyright-year").innerHTML = YEAR_SPAN;
+    /*set value from cookie*/
+    setValueFromCookie();
+    if(formMinMaxCheck() && !checkEmpty()) {
+        document.getElementById("rand-button").disabled = false;
+    }else{
+        document.getElementById("rand-button").disabled = true;
+    }
     /*button event*/
     document.getElementById("rand-button").addEventListener("click", buttonClick);
     /*form check*/
@@ -173,7 +245,10 @@ window.onload = function() {
         }
     });
     /*test code*/
-    document.getElementById("test").addEventListener("click", function() {
-        //document.getElementById("min").classList.add("error");
-    });
+    // document.getElementById("test").addEventListener("click", function() {
+    //     getCookie();
+    // });
+    // document.getElementById("test2").addEventListener("click", function() {
+    //     deleteCookie();
+    // });
 }
